f5afa985b6a3442ec1411e6352fa819bcde5bb01
Gajula <pranava.gajula@dxc.com>
2023-04-21 11:12:02 +0530

    add_5_digit

---
+++ CSharp/add_5_digit/add_5_digit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+int i;
+Console.WriteLine("enter a number");
+i = Convert.ToInt16(Console.ReadLine());
+if (i>9999 && i<100000)
+{
+   int Sum = 0;
+    while (i != 0)
+    {
+        Sum += i % 10;
+        i = i / 10;
+    }
+    Console.WriteLine($"sum of digits {Sum}");
+}
+else
+{
+    Console.WriteLine("Enter 5 digit number only");
+}
